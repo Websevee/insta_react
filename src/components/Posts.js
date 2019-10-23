e1e39fe948054d7095ab80714302b141ec2c5dc4
Websevee <Websevee@gmail.com>
2019-10-24 00:10:29 +0300

    Day 2

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,72 @@
 import React, {Component} from 'react';
-import Post from './Post'
-
+import InstaService from '../services/instaService';
+import User from './User';
+import ErrorMessage from './Error';
 export default class Posts extends Component {
+    InstaService = new InstaService();
+    state = {
+        posts: [],
+        error: false
+    }
+
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts() {
+        this.InstaService.getAllPosts()
+        .then(this.onPostsLoaded)
+        .catch(this.onError);
+    }
+
+    onPostsLoaded = (posts) => {
+        this.setState({
+            posts,
+            error: false
+        });
+        console.log(this.state.posts);
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map(item => {
+            const {name, altname, photo, src, alt, descr, id} = item;
+
+            return (
+                <div key={id} className="post">
+                    <User 
+                        src={photo}
+                        alt={altname}
+                        name={name}
+                        min/>
+                    <img src={src} alt={alt}></img>
+                    <div className="post__name">
+                        {name}
+                    </div>
+                    <div className="post__descr">
+                        {descr}
+                    </div>
+                </div>
+            )
+        });
+    }
+
     render() {
+        const {error, posts} = this.state;
+
+        if (error) {
+            return <ErrorMessage/>
+        }
+
+        const items = this.renderItems(posts);
         return (
             <div className="left">
-                <Post src="https://images.unsplash.com/photo-1496857239036-1fb137683000?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="inst"/>
-                <Post src="http://storage.inovaco.ru/media/project_mo_175/a0/03/24/db/61/67/vfr.jpg" alt="second"/>
+                {items}
             </div>
         )
     }
